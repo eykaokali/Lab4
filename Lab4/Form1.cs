6056454eacb3a4c9e056e715943e8aff52cc8887
Lisuko Лисуко <liza.shevchuk08@gmail.com>
2025-04-24 14:06:22 +0300

    Add project files.

---
+++ Lab4/Form1.cs
@@ -0,0 +1,116 @@
+﻿using Lab4;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            gvTV.AutoGenerateColumns = false;
+
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Model";
+            column.Name = "Модель";
+            gvTV.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Brand";
+            column.Name = "Бренд";
+            gvTV.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "ScreenSize";
+            column.Name = "Розмір";
+            gvTV.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Resolution";
+            column.Name = "Розширення";
+            gvTV.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "IsSmart";
+            column.Name = "Смарт";
+            gvTV.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Color";
+            column.Name = "Колір";
+            gvTV.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Price";
+            column.Name = "Ціна";
+            gvTV.Columns.Add(column);
+
+            bindSrcTV.Add(new TV("A21", "Samsung", 50, "1024x256", false, "чорний", 80000));
+            EventArgs args = new EventArgs(); OnResize(args);
+
+        }
+
+        private void fMain_Resize(object senderun, EventArgs e)
+        {
+            int buttonsSize = 5 * btnAdd.Width + 2 * tsSeparator1.Width + 30;
+            btnExit.Margin = new Padding(Width - buttonsSize, 0, 0, 0);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TV tv = new TV();
+            fTV fTV = new fTV(ref tv);
+            if (fTV.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcTV.Add(tv);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            TV tv = (TV)bindSrcTV.List[bindSrcTV.Position];
+
+            fTV fTV = new fTV(ref tv);
+            if (fTV.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcTV.List[bindSrcTV.Position] = tv;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Видалити поточний запис?", "Видалення запису", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                bindSrcTV.RemoveCurrent();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Очистити таблицю?\n\nВсі дані будуть втрачені", "Очищення даних", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                bindSrcTV.Clear();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Закрити застосунок?", "Вихід з програми", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
